a033da3f1595b3fff00d0765d5983f0e6830ee06
RadkaSed <sed.radka@seznam.cz>
2024-11-24 21:46:58 +0100

    Optimize code, add data validation for valid games

--- 2.py
+++ 2.py
@@ -1,49 +1,60 @@
 games = {}
 
 with open('input_2.txt', mode='r', encoding='utf-8') as input_file:
-    content = input_file.readlines()
+    content = (line.strip() for line in input_file if line.strip())
+
+
     for line in content:
-        line = line.strip()
-        if line:
-            game_num, game_content = line.split(':')
-            game_number = int(game_num.split()[1])
-            game_part = game_content.split(';')
-            game_dict = []
-            for part in game_part:
-                part_dict = {}
-                items = part.split(',')
-                for item in items:
-                    colour_value = item.split()
-                    if len(colour_value) == 2:
-                        value = int(colour_value[0])
-                        colour = colour_value[1]
-                        part_dict[colour] = value
-                game_dict.append(part_dict)
-            games[game_number] = game_dict
-            
+        game_num, game_content = line.split(':')
+        game_number = int(game_num.split()[1])
+        game_part = game_content.split(';')
+        game_dict = []
+        for part in game_part:
+            part_dict = {}
+            items = part.split(',')
+            for item in items:
+                value, colour = item.split()
+                part_dict[colour] = int(value)
+            game_dict.append(part_dict)
+        
+        games[game_number] = game_dict
+        
+
 real_cubes = {
     'red': 12,
     'green': 13,
     'blue': 14
 }
-        
-real_games = []
 
-for game_num, game_cubes in games.items():
-    valid = True 
+def is_valid_game(game_cubes, real_cubes):
     for part_dict in game_cubes:
         for colour, value in part_dict.items():
             if colour in real_cubes and value > real_cubes[colour]:
-                valid = False 
-                break
-        if not valid:
-            break
-    if valid:
-        real_games.append(game_num)
-            
-print(real_games)
-            
-            
-            
-sum_of_real_ID = sum(real_games)
-print(sum_of_real_ID)
+                return False
+    return True
+
+
+real_games = [game_num for game_num, game_cubes in games.items() if is_valid_game(game_cubes, real_cubes)]
+
+games_cubes_multiplies = []
+
+min_cubes_for_game = []  
+
+for game_num, game_content in games.items():
+    game_min_cubes = {}
+    min_cubes = {'blue': 0, 'red': 0, 'green': 0}
+    for part_dict in game_content:
+        for colour, value in part_dict.items():
+            if colour in min_cubes and part_dict[colour] > min_cubes[colour]:
+                min_cubes[colour] = value
+    game_min_cubes[game_num] = min_cubes     
+    min_cubes_for_game.append(game_min_cubes)
+    
+for game in min_cubes_for_game:
+    for game_num, game_content in game.items():
+        multiply_result = 1
+        for colour, value in game_content.items():
+            multiply_result *= value
+        games_cubes_multiplies.append(multiply_result)
+        
+print(sum(games_cubes_multiplies))
